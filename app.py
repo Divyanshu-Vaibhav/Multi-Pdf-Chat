b8576f5ef52d588662530eb82bc36bb46eefce0b
Divyanshu-Vaibhav <divyanshuvaibhav050603@gmail.com>
2025-03-28 01:02:15 +0530

    Handled user input through our conversation chain and displayed our response, used ChatOpenAi model here!

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
 
-
-load_dotenv()
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -40,16 +39,37 @@ def get_conversation_chain(vectorstore):
     conversaton_chain = ConversationalRetrievalChain.from_llm(llm=llm,retriever=vectorstore.as_retriever(),memory=memory)
     return conversaton_chain
     
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question':user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i%2==0:    
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
 
 
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
     st.header("Chat with multiple PDFs :books:")
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
-    st.text_input("Ask a question about your documents:")
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+
+
+
+
+
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your docs here and click on Process",accept_multiple_files=True)
@@ -64,7 +84,7 @@ def main():
                 #create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
-    st.session_state.conversation
+    # st.session_state.conversation
 
 if __name__== '__main__':
     main()
