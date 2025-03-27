65cca7c6da063e1dfb72167034d9ab1dc8f17023
Divyanshu-Vaibhav <divyanshuvaibhav050603@gmail.com>
2025-03-28 00:02:51 +0530

    added requirements file

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
-import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
 
 load_dotenv()
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -30,9 +34,21 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks,embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+    conversaton_chain = ConversationalRetrievalChain.from_llm(llm=llm,retriever=vectorstore.as_retriever(),memory=memory)
+    return conversaton_chain
+    
+
+
+
+
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs",page_icon=":books:")
     st.header("Chat with multiple PDFs :books:")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
     st.text_input("Ask a question about your documents:")
     with st.sidebar:
         st.subheader("Your documents")
@@ -45,6 +61,10 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
+                #create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+    st.session_state.conversation
 
 if __name__== '__main__':
     main()
